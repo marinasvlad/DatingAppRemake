260cb4fcc6db33c9491a1a3e1255b36b4ef2b2de
vladmarinas <vlad.marinas@listafirme.onmicrosoft.com>
2023-11-16 17:48:32 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,6 +29,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
