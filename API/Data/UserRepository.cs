3544fe02f35077867bad054917c8bc894716da9b
Vlad Marinas <marinasvlad@gmail.com>
2023-12-30 22:02:21 +0200

    End of section 13

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -21,11 +21,24 @@ public class UserRepository : IUserRepository
 
     public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
     {
-        var query = _context.Users
-        .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-        .AsNoTracking();
+        var query = _context.Users.AsQueryable();
 
-        return await PagedList<MemberDto>.CreateAsync(query, userParams.pageNumber, userParams.PageSize);        
+        query = query.Where(u => u.UserName != userParams.CurrentUsername);
+        query = query.Where(u => u.Gender == userParams.Gender);
+
+        var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+        var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+
+        query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+        query = userParams.OrderBy switch
+        {
+           "created" => query.OrderByDescending(u => u.Created),
+           _ => query.OrderByDescending(u => u.LastActive)
+        };
+
+        return await PagedList<MemberDto>.CreateAsync(
+        query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider),
+        userParams.pageNumber, userParams.PageSize);        
     }
 
     public async Task<MemberDto> GetMembrAsync(string username)
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { Pagination } from 'src/app/_models/pagination';
+import { User } from 'src/app/_models/user';
+import { UserParams } from 'src/app/_models/userParams';
+import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -12,9 +15,13 @@ import { MembersService } from 'src/app/_services/members.service';
 export class MemberListComponent implements OnInit {
   members: Member[] = [];
   pagination: Pagination | undefined;
-  pageNumber = 1;
-  pageSize = 5;
-  constructor(private memberService: MembersService) {  }
+  userParams: UserParams | undefined;
+  user: User | undefined;
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+
+  constructor(private memberService: MembersService, private accountService: AccountService) {
+      this.userParams = this.memberService.getUserParams();
+    }
 
   ngOnInit(): void {
     //this.members$ = this.memberService.getMembers();
@@ -22,13 +29,29 @@ export class MemberListComponent implements OnInit {
   }
 
   loadMembers(){
-    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
-      next: response => {
-        if(response.result && response.pagination){
-          this.members = response.result;
-          this.pagination = response.pagination;
+    if(this.userParams){
+      this.memberService.setUserParams(this.userParams);
+      this.memberService.getMembers(this.userParams).subscribe({
+        next: response => {
+          if(response.result && response.pagination){
+            this.members = response.result;
+            this.pagination = response.pagination;
+          }
         }
-      }
-    })
+      })
+    }
+  }
+
+  resetFilters(){
+      this.userParams = this.memberService.resetUserParams();
+      this.loadMembers();
+  }
+
+  pageChanged(event: any){
+    if(this.userParams && this.userParams?.pageNumber !== event.page){
+      this.userParams.pageNumber = event.page;
+      this.memberService.setUserParams(this.userParams);
+      this.loadMembers();
+    }
   }
 }
