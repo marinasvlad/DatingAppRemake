4410b3614b9ebb417283ca7178c13906c893b53a
Vlad Marinas <marinasvlad@gmail.com>
2024-01-02 21:01:47 +0200

    Made Some Change

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,7 +11,7 @@ public class DataContext : DbContext
 
     public DbSet<AppUser> Users {get; set;}
 
-    public DbSet<UserLike> TableLike { get; set; }
+    public DbSet<UserLike> Likes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder){
         base.OnModelCreating(builder);
