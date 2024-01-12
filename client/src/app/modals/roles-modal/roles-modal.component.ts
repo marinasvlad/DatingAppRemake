3e9e9174e782e963ec68c8cb499fa4c784d6314c
vladmarinas <vlad.marinas@listafirme.onmicrosoft.com>
2024-01-12 11:50:17 +0200

    End of section 16

---
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-roles-modal',
+  templateUrl: './roles-modal.component.html',
+  styleUrls: ['./roles-modal.component.css']
+})
+export class RolesModalComponent implements OnInit {
+  username =  '';
+  availableRoles: any[] = [];
+  selectedRoles: any[] = [];
+
+  constructor(public bsModalRef: BsModalRef) { }
+
+  ngOnInit(): void {
+  }
+
+  updateChecked(checkedvalue: string){
+    const index = this.selectedRoles.indexOf(checkedvalue);
+    index !== -1 ? this.selectedRoles.splice(index, 1) : this.selectedRoles.push(checkedvalue);
+  }
+}
